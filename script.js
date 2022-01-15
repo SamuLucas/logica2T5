c3de588b7a7f9cd288d131722caf15972856dd3d
SamuLucas <samulucasf@hotmail.com>
2022-01-14 23:01:01 -0300

    Exercicos estruturas condicionais

---
+++ script.js
@@ -0,0 +1,37 @@
+//Exercícios complementares
+
+let idade = 25
+
+if (idade > 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
+
+let aniversario = "agosto";
+
+
+if (aniversario == "janeiro" || "Dezembro") {
+    console.log("Parabéns aniversariante!")
+} else {
+    console.log("Espere mais um pouco pra comemorar!")
+}
+
+//Exercícios Bônus
+
+let primeiraLetra = "S"
+
+if (primeiraLetra == "R") {
+    console.log("Seu nome começa com a letra R")
+} else {
+    console.log("Seu nome não começa com a letra R")
+}
+
+let ferreira = 8;
+let letra = "S"
+
+if (ferreira > 6 || letra == "E") {
+    console.log("A sentença é verdadeira")
+} else {
+    console.log("A sentença é falsa")
+}
